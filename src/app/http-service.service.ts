183fb2a73dd476c8ba7f50722ac0eec6d3653bcf
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-30 21:25:41 -0600

    Creando método para obtener los datos en el servicio

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  constructor(private htpp: HttpClient) { }
+  constructor(private htpp: HttpClient) {}
+
+  getData() {
+    this.htpp.get(`${environment.endpoint}app/api/empleado`).subscribe((data) => data);
+  }
 }
