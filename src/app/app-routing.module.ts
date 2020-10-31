a375765c6221a15c987143242737a7aa711daf42
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-30 21:10:11 -0600

    Configurando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { ListComponent } from './pages/list/list.component';
+import { FormComponent } from './pages/form/form.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapaComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+  { path: '**', redirectTo: 'map', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
