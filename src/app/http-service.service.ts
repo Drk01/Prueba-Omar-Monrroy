528c335ba36adef969c6e53923468e1996b9d14b
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-31 02:50:17 -0600

    Terminando sección para traer información

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HttpServiceService {
 
-  constructor(private htpp: HttpClient) {}
+  constructor(private htpp: HttpClient) { }
 
   getData() {
-    this.htpp.get(`${environment.endpoint}app/api/empleado`).subscribe((data) => data);
+    return this.htpp
+      .get(`${environment.endpoint}/app/api/empleado`)
   }
 }
--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from "./../../http-service.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private httpService: HttpServiceService) {}
+  data;
   ngOnInit() {
+    this.fetchData();
   }
 
+  fetchData() {
+    this.httpService.getData().subscribe((data) => {
+      this.data = data;
+
+      console.log(this.data);
+    });
+  }
 }
