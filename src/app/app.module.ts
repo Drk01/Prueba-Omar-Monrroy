e8bdb2109fb37528d4d6aa41e2c2c93a09bd52aa
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-30 21:02:48 -0600

    Creando componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
 import { ListComponent } from './pages/list/list.component';
 import { FormComponent } from './pages/form/form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapaComponent,
     ListComponent,
-    FormComponent
+    FormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
