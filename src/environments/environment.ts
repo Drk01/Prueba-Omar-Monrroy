8a4bcc11bb06f10180ec8677bf6cf65631d37cee
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-31 02:45:27 -0600

    Eliminando / del path base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'https://us-central1-sctdgc.cloudfunctions.net/'
+  endpoint: 'https://us-central1-sctdgc.cloudfunctions.net'
 };
 
 /*
